8cdba356b9a1b2a29abbb68860641df34a06d93b
Thousifg <thousifg1999@gmail.com>
2022-05-18 22:52:00 +0530

    done

---
+++ src/redux/reducers/findFalconeReducer.js
@@ -0,0 +1,17 @@
+import { createReducer } from "@reduxjs/toolkit";
+import { findFalconeAction, fetchTokenAction } from "../actions/actions";
+
+const reducer = createReducer(
+  {},
+  {
+    [findFalconeAction.type]: (findFalcone, action) => {
+      findFalcone.result = action.payload;
+    },
+
+    [fetchTokenAction.type]: (findFalcone, action) => {
+      findFalcone.token = action.payload;
+    },
+  }
+);
+
+export default reducer;
---
+++ src/redux/reducers/vehiclesReducer.js
@@ -0,0 +1,13 @@
+import { createReducer } from "@reduxjs/toolkit";
+import { fetchVehiclesAction } from "../actions/actions";
+
+const reducer = createReducer([], {
+  [fetchVehiclesAction.type]: (vehicles, action) => {
+    vehicles.length = 0; //resetting the array
+    action.payload.forEach((veh) => {
+      vehicles.push(veh);
+    });
+  },
+});
+
+export default reducer;

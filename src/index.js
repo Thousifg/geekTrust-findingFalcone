ea3e0be018cad55df3f116941d47e71d5e6d27ba
Thousifg <thousifg1999@gmail.com>
2022-05-18 22:53:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Routers from './Routers/Routers'
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Routers from "./Routers/Routers";
+import * as serviceWorker from "./serviceWorker";
 import configureStore from "./redux/configureStore";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 
 const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store = {store}>
-    <Routers />
+    <Provider store={store}>
+      <Routers />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
